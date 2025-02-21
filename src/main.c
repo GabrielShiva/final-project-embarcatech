ae606cdf7c3a911250f853693a6a0ef1e15ff28e
Gabriel Shiva Gomes Oliveira <gabrielshivagomes@gmail.com>
2025-02-21 00:34:42 -0300

    adiciona as telas de def. de nivel e de medição além de criar nova tela (conf.)

--- src/main.c
+++ src/main.c
@@ -18,25 +18,29 @@
 #define BTN_SW 22
 
 volatile uint32_t last_time_btn_press = 0;
+volatile uint16_t current_db_value = 10;
+char db_string[10];
 
 // inicia a estrutura do display OLED
 ssd1306_t ssd;
 
 /**
  *  0 => item de vizualização
- *  1 => item de configuração
+ *  1 => item de definir nível
+ *  2 => item de configuração
  */
 static volatile uint current_menu_item = 0;
 
 /**
  *  0 => página do menu principal
  *  1 => página de vizualização 
- *  2 => página de configuração
+ *  2 => página de definir nível
+ *  3 => página de configuração
  */
 static volatile uint current_screen = 0;
 
 const char *menu_itens[] = {
-    "VIZUALIZAR", "CONFIGURAR"
+    "VIZUALIZAR", "DEF NIVEL", "CONFIGURAR"
 };
 
 // configuração do protocolo i2c
@@ -105,7 +109,22 @@ void inital_screen() {
     ssd1306_pixel(&ssd, 125, 12, true);
 
     ssd1306_vline(&ssd, 78, 0, 13, true);
-    ssd1306_vline(&ssd, 79, 0, 13, true);
+
+    ssd1306_pixel(&ssd, 76, 1, true);
+    ssd1306_pixel(&ssd, 77, 1, true);
+    ssd1306_pixel(&ssd, 77, 2, true);
+
+    ssd1306_pixel(&ssd, 76, 12, true);
+    ssd1306_pixel(&ssd, 77, 12, true);
+    ssd1306_pixel(&ssd, 77, 11, true);
+
+    ssd1306_pixel(&ssd, 79, 1, true);
+    ssd1306_pixel(&ssd, 79, 2, true);
+    ssd1306_pixel(&ssd, 80, 1, true);
+
+    ssd1306_pixel(&ssd, 79, 12, true);
+    ssd1306_pixel(&ssd, 79, 11, true);
+    ssd1306_pixel(&ssd, 80, 12, true);
 
     // ssd1306_rect(&ssd, 14, 3, 5, 5, true, true);
     ssd1306_draw_string(&ssd, "Inicializando", 3, 25); // Desenha uma string
@@ -128,6 +147,34 @@ void draw_right_arrow() {
     ssd1306_vline(&ssd, 117, 37, 38, true);
 }
 
+void draw_plus_btn() {
+    // box
+    ssd1306_rect(&ssd, 105, 28, 16, 16, true, true);
+    // x
+    ssd1306_vline(&ssd, 112, 30, 41, false);
+    ssd1306_vline(&ssd, 113, 30, 41, false);
+    // y
+    ssd1306_hline(&ssd, 107, 118, 35, false);
+    ssd1306_hline(&ssd, 107, 118, 36, false);
+}
+
+void draw_minus_btn() {
+    // box
+    ssd1306_rect(&ssd, 11, 28, 16, 16, true, true);
+    // x
+    ssd1306_hline(&ssd, 13, 24, 35, false);
+    ssd1306_hline(&ssd, 13, 24, 36, false);
+}
+
+void draw_back_arrow() {
+    ssd1306_vline(&ssd, 92, 54, 61, true);
+    ssd1306_vline(&ssd, 91, 55, 60, true);
+    ssd1306_vline(&ssd, 90, 56, 59, true);
+    ssd1306_vline(&ssd, 89, 57, 58, true);
+
+    ssd1306_draw_string(&ssd, "vltr", 95, 54);
+}
+
 void irq_handler(uint gpio, uint32_t events) {
     uint32_t current_time = to_ms_since_boot(get_absolute_time());
 
@@ -135,12 +182,26 @@ void irq_handler(uint gpio, uint32_t events) {
         last_time_btn_press = current_time;
 
         if (gpio == BTN_A) {
-            if (current_screen == 0 && current_menu_item > 0) {
-                current_menu_item = current_menu_item - 1;
+            if (current_screen == 0) {
+                if (current_menu_item > 0) {
+                    current_menu_item = current_menu_item - 1;
+                }
+            } else if (current_screen == 2) {
+                if (current_db_value > 0) {
+                    current_db_value = current_db_value - 1;
+                    printf("db: %d\n", current_db_value);
+                }
             }
         } else if (gpio == BTN_B) {
-            if (current_screen == 0 && current_menu_item < 1) {
-                current_menu_item = current_menu_item + 1;
+            if (current_screen == 0) {
+                if (current_menu_item < 2) {
+                    current_menu_item = current_menu_item + 1;
+                }
+            } else if (current_screen == 2) {
+                if (current_db_value < 20) {
+                    current_db_value = current_db_value + 1;
+                    printf("db: %d\n", current_db_value);
+                }
             }
         } else if (gpio == BTN_SW) {
             if (current_screen == 0) {
@@ -149,9 +210,12 @@ void irq_handler(uint gpio, uint32_t events) {
                     printf("TELA DE VIZUALIZACAO\n");
                 } else if (current_menu_item == 1) {
                     current_screen = 2;
+                    printf("TELA DE DEF NIVEL\n");
+                } else if (current_menu_item == 2) {
+                    current_screen = 3;
                     printf("TELA DE COFIGURACAO\n");
                 }
-            } else if (current_screen == 1 || current_screen == 2) {
+            } else if (current_screen != 0) {
                 current_screen = 0;
                 printf("VOLTANDO PARA MENU PRINCIPAL\n");
             }
@@ -174,6 +238,8 @@ int main() {
 
     sleep_ms(1500);
 
+    snprintf(db_string, sizeof(db_string), "%ddB", current_db_value);
+    ssd1306_draw_string(&ssd, db_string, 82, 3); // Desenha uma string
     ssd1306_rect(&ssd, 0, 14, 128, 50, false, true);
     // atualiza o display OLED
     
@@ -200,14 +266,42 @@ int main() {
             } else if (current_menu_item == 1) {
                 ssd1306_draw_string(&ssd, menu_itens[current_menu_item], 25, 34); // Desenha uma string
                 draw_left_arrow();
+                draw_right_arrow();
+                ssd1306_send_data(&ssd);
+            } else if (current_menu_item == 2) {
+                ssd1306_draw_string(&ssd, menu_itens[current_menu_item], 25, 34); // Desenha uma string
+                draw_left_arrow();
                 ssd1306_send_data(&ssd);
             }
-
         } else if (current_screen == 1) { // vizualização
+            ssd1306_rect(&ssd, 0, 14, 128, 50, false, true);
+            draw_back_arrow();
+            // progress bar
+            ssd1306_rect(&ssd, 0, 20, 82, 16, true, false);
+            ssd1306_rect(&ssd, 0, 20, 50, 16, true, true);
+            ssd1306_draw_string(&ssd, "134dB", 84, 25); // valor medido em tempo real
+            ssd1306_draw_string(&ssd, "MEDIANO", 0, 40); // valor medido em tempo real
 
-        } else if (current_screen == 2) { // configuração
+            ssd1306_send_data(&ssd);
+        } else if (current_screen == 2) { // definir nível
+            ssd1306_rect(&ssd, 0, 14, 128, 50, false, true);
+            draw_back_arrow();
+            draw_plus_btn();
+            draw_minus_btn();
 
+            snprintf(db_string, sizeof(db_string), "%ddB", current_db_value);
+
+            ssd1306_draw_string(&ssd, db_string, 44, 33);
+            ssd1306_send_data(&ssd);
+        } else if (current_screen == 3) { // configuração
+            ssd1306_rect(&ssd, 0, 14, 128, 50, false, true);
+            draw_back_arrow();
+            ssd1306_send_data(&ssd);
         }
+
+        ssd1306_rect(&ssd, 79, 1, 45, 11, false, true);
+        snprintf(db_string, sizeof(db_string), "%ddB", current_db_value);
+        ssd1306_draw_string(&ssd, db_string, 82, 3); // Desenha uma string
 
         sleep_ms(500);
     }
